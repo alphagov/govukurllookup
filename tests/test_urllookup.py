9f3abcb169f431d2fe6c5137377c005074a45ce6
Ellie King <ellie.king@digital.cabinet-office.gov.uk>
2017-08-10 18:30:28 +0100

    Start converting testing script from unittest to pytest

--- tests/test_urllookup.py
+++ tests/test_urllookup.py
@@ -1,10 +1,31 @@
+# coding: utf-8
+
 import unittest
-from urllookup import *
+import pytest
+
 import pandas as pd
+import re, requests
+import sys
+sys.path.append('/Users/ellieking/govuk_api')
+from govukurllookup import *
+
+def test_api_lookup():
+    r = requests.get('https://www.gov.uk/api/content/government/news/national-apprenticeship-award-winners-announced')
+
+    expected = r.json()
+
+    assert api_lookup('/government/news/hertfordshire-apprentice-wins-national-apprenticeship-award') == expected
+
+def test_govukurls_class_init(self):
+
+    assert (len(self.urlsclass.dedupurls) < len(self.urls))
+    assert (len(self.urlsclass.dedupurls) == len(self.urls) / 2)
+
+
 
 class TestCleanUrls(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self):#runs everytime a test id run
         
         # Load in test data
 
@@ -29,33 +50,7 @@ class TestCleanUrls(unittest.TestCase):
 #    def tearDown(self):
 #
     
-    def test_govukurls_class_init(self):
 
-        self.assertTrue(len(self.urlsclass.dedupurls) < len(self.urls))
-        self.assertTrue(len(self.urlsclass.dedupurls) == len(self.urls) / 2)
 
-    def test_clean_url(self):
-    
-        self.assertTrue(clean_url(self.urls[0]) == {'full_url': '/', 'page': '/', 'org0': None, 'section0': 'site-nav', 'section1': 'site-nav'})
-        self.assertTrue(clean_url(self.urls[1]) == {'full_url': '/government/world/turkey', 'page': '/government/world', 'org0': 'Foreign & Commonwealth Office'})
-        self.assertTrue(clean_url(self.urls[2]) == {'full_url': '/government/publications/crown-commercial-service-customer-update-september-2016/crown-commercial-service-update-september-2016', 'page': '/government/publications/crown-commercial-service-customer-update-september-2016/crown-commercial-service-update-september-2016', 'org0': None})
-        self.assertTrue(clean_url(self.urls[3]) == {'full_url': '/guidance/guidance-for-driving-examiners-carrying-out-driving-tests-dt1/05-candidates-with-an-impairment', 'page': '/guidance/guidance-for-driving-examiners-carrying-out-driving-tests-dt1/05-candidates-with-an-impairment', 'org0': None})
-        self.assertTrue(clean_url(self.urls[4]) == {'full_url': '/search/this-is/a-search/url', 'page': '/search/this-is/a-search/url', 'org0': None, 'section0': 'site-nav', 'section1': 'site-nav'})
-        self.assertTrue(clean_url(self.urls[5]) == {'full_url': '/help/this/is/a/help/url', 'page': '/help/this/is/a/help/url', 'org0': None, 'section0': 'site-nav', 'section1': 'site-nav'})
-        self.assertTrue(clean_url(self.urls[6]) == {'full_url': '/contact/this/is/a/contact/url', 'page': '/contact/this/is/a/contact/url', 'org0': None, 'section0': 'contact', 'section1': 'contact'})
 
-    # TODO: Expand clean and lookup tests
-
-    def test_clean_method(self):
-
-        self.urlsclass.clean()
-        self.assertTrue(isinstance(self.urlsclass.cleanurls, list))
-        self.assertTrue(isinstance(self.urlsclass.cleanurls[0], dict))
-
-    def test_lookup_method(self):
-        
-        self.urlsclass.clean()
-        self.urlsclass.lookup()
-        self.assertTrue(isinstance(self.urlsclass.urlsdf, pd.core.frame.DataFrame))
-        self.assertTrue(self.urlsclass.urlsdf.loc[3,'org0'] == 'Driver and Vehicle Standards Agency')  
 
