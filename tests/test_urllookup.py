7f490c2c801e129883804769406d705019b3963d
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-11-08 18:56:10 +0000

    Revert "Skip extract_texts() tests if not python2.7"
    This has now been fixed in 69465cc6b524672d85dc25c32ce8c15ee47896aa.
    
    This reverts commit 22fc60bfff58a9dd29291df915d837be90e8c542.

--- tests/test_urllookup.py
+++ tests/test_urllookup.py
@@ -4,7 +4,6 @@
 
 import pytest
 import requests
-import sys
 from govukurllookup import api_lookup, govukurls
 import pandas as pd
 
@@ -88,7 +87,6 @@ class TestGovukurls(object):
 
         # TODO: test a redirect url
 
-    @pytest.mark.skipif(sys.version_info >= (3,6), reason="requires python 2.7")
     def test_extract_text(self):
         """
         Test the extract_text() method.
